48a8540d697c0bacaec89597d85bd1596919d684
Mustafoyev <otabekshark@gmail.com>
2022-09-11 11:41:29 +0500

    Add hero section

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var elMenu = document.querySelector('.navbar');
+var elBtn = document.querySelector('.burger');
+
+elBtn.addEventListener('click', function () {
+	elMenu.classList.toggle('menu');
+});
